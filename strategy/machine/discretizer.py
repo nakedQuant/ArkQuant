af66e7e195dd8252e7319ffe8e9031e9df9de3c7
macpython <13776668123@163.com>
2020-08-24 18:12:18 +0800

    对应 strategy模块内的文件进行相关调整，适配

--- strategy/machine/discretizer.py
+++ strategy/machine/discretizer.py
@@ -5,16 +5,16 @@ Created on Tue Mar 12 15:37:47 2019
 
 @author: python
 """
-from strategy import Strategy
-
-
-class Discretizer(Strategy):
-    """
-        通用模块 close - pre_high
-    """
-
-    def __init__(self, params):
-        self.params = params
-
-    def _compute(self, data, mask):
-        raise NotImplementedError()
+# from strategy import Strategy
+#
+#
+# class Discretizer(Strategy):
+#     """
+#         通用模块 close - pre_high
+#     """
+#
+#     def __init__(self, params):
+#         self.params = params
+#
+#     def _compute(self, data, mask):
+#         raise NotImplementedError()
