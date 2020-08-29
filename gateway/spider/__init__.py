8147efd1132b09d5dc946efaa8da15757bdb22b0
macpython <13776668123@163.com>
2020-08-29 11:33:14 +0800

    spider经过测试OK

--- gateway/spider/__init__.py
+++ gateway/spider/__init__.py
@@ -12,28 +12,7 @@ from sqlalchemy import select, func
 from gateway.database import metadata, engine
 
 
-__all__ = ['UserAgent', 'ProxyIp', 'Crawler']
-
-
-UserAgent = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 '
-             '(KHTML, like Gecko) Version/13.1.2 Safari/605.1.15',
-             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 '
-             '(KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-             'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 '
-             '(KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-            ]
-
-ProxyIp = ['http://0825fq11t1m612:0825fq11t1m612@117.40.5.82:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.7.122:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.5.35:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.5.128:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.6.202:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.5.94:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.5.28:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.7.9:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.7.100:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.7.200:65000',
-           'http://0825fq11t1m612:0825fq11t1m612@117.40.7.165:65000', None]
+__all__ = ['Crawler']
 
 
 class Crawler(ABC):
